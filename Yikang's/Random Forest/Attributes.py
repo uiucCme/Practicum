b669f5007aca08288f9beb2de9246b12d833c953
luoy2 <luoy2@hotmail.com>
2016-10-12 21:18:42 -0500

    fixed bug

--- Yikang's/Random Forest/Attributes.py
+++ Yikang's/Random Forest/Attributes.py
@@ -8,22 +8,29 @@ import matplotlib.pyplot as plt
 import h5py
 
 
-# function to extract data in time range needed, and delete all 0 difference data
+# function to extract data in time range needed, and delete all 0
+# difference data
 def data_cleaning(df_input):
     start_time = '10:30:00'
     end_time = '15:00:00'
     ftr = [3600, 60, 1]
-    start_timestamp = sum([a * b for a, b in zip(ftr, [int(i) for i in start_time.split(":")])]) * 1e+9
-    end_timestamp = sum([a * b for a, b in zip(ftr, [int(i) for i in end_time.split(":")])]) * 1e+9
+    start_timestamp = sum([a * b for a, b in zip(ftr, [int(i)
+                          for i in start_time.split(":")])]) * 1e+9
+    end_timestamp = sum([a * b for a, b in zip(ftr, [int(i)
+                        for i in end_time.split(":")])]) * 1e+9
 
-    execution_message = df_input[df_input[1].isin([ord('E'), ord('C')])].reset_index(drop=True)
-    execution_message = execution_message[start_timestamp <= execution_message[0]]
-    execution_message = execution_message[execution_message[0] <= end_timestamp].reset_index(drop=True)
+    execution_message = df_input[df_input[1].isin(
+        [ord('E'), ord('C')])].reset_index(drop=True)
+    execution_message = execution_message[
+        start_timestamp <= execution_message[0]]
+    execution_message = execution_message[
+        execution_message[0] <= end_timestamp].reset_index(drop=True)
 
     execution_time = np.array(execution_message[0])
     diff_time = np.diff(execution_time)
     diff_time = np.insert(diff_time, 0, np.nan)
-    execution_message = execution_message[diff_time != 0].reset_index(drop=True)
+    execution_message = execution_message[
+        diff_time != 0].reset_index(drop=True)
     return (execution_message)
 
 
@@ -34,8 +41,10 @@ def get_direction(input_val):
         return 1
     elif input_val == 0:
         return 0
-    else:
+    elif input_val < 0:
         return -1
+    else:
+        return np.nan
 
 
 def get_imbalance_rate(df_input, level):
@@ -44,14 +53,13 @@ def get_imbalance_rate(df_input, level):
             bid_column += df_input['best.bid.q.' + str(i)]
             ask_column += df_input['best.ask.q.' + str(i)]
         except:
-            bid_column = df_input['best.bid.q.1']
-            ask_column = df_input['best.ask.q.1']
+            bid_column = df_input['best.bid.q.1'].copy()
+            ask_column = df_input['best.ask.q.1'].copy()
     return bid_column / ask_column
 
 
-os.chdir("D:/SkyDrive/Documents/UIUC/CME Fall 2016")
-
-# os.chdir("/Users/luoy2/OneDrive/Documents/UIUC/CME Fall 2016")
+# os.chdir("D:/SkyDrive/Documents/UIUC/CME Fall 2016")
+os.chdir("/Users/luoy2/OneDrive/Documents/UIUC/CME Fall 2016")
 data_input_dir = os.getcwd() + "/data/order_book/"
 data_output_dir = os.getcwd() + "/data/random_forest/"
 
@@ -72,14 +80,17 @@ execution_record_df = data_cleaning(record_df)
 
 
 # Start build training attributes
-execution_orderbook_df.drop([0, 1, 22], 1, inplace=True)  # no need for time, message type and bid ask divide
-execution_record_df.drop([0, 1], 1, inplace=True)  # only need price and quantity
+# no need for time, message type and bid ask divide
+execution_orderbook_df.drop([0, 1, 22], 1, inplace=True)
+# only need price and quantity
+execution_record_df.drop([0, 1], 1, inplace=True)
 execution_record_pctchange_df = execution_record_df.pct_change(1)
 execution_orderbook_pctchange_df = execution_orderbook_df.pct_change(1)
 
 execution_record_df.columns = ['exe.p', 'exe.v']
 execution_record_pctchange_df.columns = ['exe.p.d', 'exe.v.c']
-execution_record_pctchange_df['exe.p.d'] = execution_record_pctchange_df['exe.p.d'].apply(get_direction)
+execution_record_pctchange_df['exe.p.d'] = execution_record_pctchange_df[
+    'exe.p.d'].apply(get_direction)
 
 order_book_column = []
 for i in reversed(range(1, 11)):
@@ -102,7 +113,8 @@ execution_orderbook_pctchange_df.columns = order_book_pctc_column
 # caluclate the imbalance rate
 imbalance_df = pd.DataFrame(index=range(len(execution_orderbook_df)))
 for i in range(1, 11):
-    imbalance_df['imbalance.' + str(i)] = get_imbalance_rate(execution_orderbook_df, i)
+    imbalance_df['imbalance.' +
+        str(i)] = get_imbalance_rate(execution_orderbook_df, i)
 
 # merge output
 output_df = execution_record_pctchange_df
@@ -113,7 +125,7 @@ output_df = output_df.join(imbalance_df)
 output_df.to_csv(data_output_dir + "/Attributes10_12.csv")
 
 output_matrix = output_df.as_matrix()
-f['SPY/Attributes'] = output_matrix
+f['SPY/Attributes']=output_matrix
 f.close()
 
 
